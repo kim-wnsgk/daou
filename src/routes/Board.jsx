6a73db1bc06e04443dfa54ea86332661b3a4c4e5
Kim Junha <kim_jh6859@naver.com>
2022-11-20 00:59:01 +0900

    게시판 수정

--- src/routes/Board.jsx
+++ src/routes/Board.jsx
@@ -6,64 +6,62 @@ function Board() {
     const [selector, setSelector] = useState('notice');
 
     return (
-        <div className={styles.container}>
-            <div className={styles.banner}>
-                logo
-            </div>
-            <div className={styles.contents}>
-                <div className={styles.normal}>
-                    <div className={styles.title}>
-                        {selector === 'notice' ? <span>공지사항</span> : selector === 'free' ? <span>자유게시판</span> : selector === 'part' ? <span>부서별게시판</span> : null}
-                    </div>
-                    <ul className={styles.posts}>
-                        <li className={styles.post}>
-                            <span style={{ flex: 0.5 }}>19:08</span>
-                            <span style={{ flex: 3 }}>제목</span>
-                            <span style={{ flex: 1, color: 'red', fontWeight: 'bold', textAlign: 'center' }}>긴급</span>
-                        </li>
-                        <li className={styles.post}>제목</li>
-                        <li className={styles.post}>제목</li>
-                        <li className={styles.post}>제목</li>
-                        <li className={styles.post}>제목</li>
-                        <li className={styles.post}>제목</li>
-                        <li className={styles.post}>제목</li>
-                        <li className={styles.post}>제목</li>
-                        <li className={styles.post}>제목</li>
-                        <li className={styles.post}>제목</li>
-                    </ul>
+        <div className={styles.contents}>
+
+            <div className={styles.normal}>
+                <div className={styles.title}>
+                    {selector === 'notice' ? <span>공지사항</span> : selector === 'free' ? <span>자유게시판</span> : selector === 'part' ? <span>부서별게시판</span> : null}
                 </div>
-                <div className={styles.sideBanner}>
+                <ul className={styles.posts}>
+                    <li className={styles.post}>
+                        <span style={{ flex: 0.5 }}>19:08</span>
+                        <span style={{ flex: 3 }}>제목</span>
+                        <span style={{ flex: 1, color: 'red', fontWeight: 'bold', textAlign: 'center' }}>긴급</span>
+                    </li>
+                    <li className={styles.post}>제목</li>
+                    <li className={styles.post}>제목</li>
+                    <li className={styles.post}>제목</li>
+                    <li className={styles.post}>제목</li>
+                    <li className={styles.post}>제목</li>
+                    <li className={styles.post}>제목</li>
+                    <li className={styles.post}>제목</li>
+                    <li className={styles.post}>제목</li>
+                    <li className={styles.post}>제목</li>
+                </ul>
+            </div>
+            <div className={styles.sideBanner}>
+                <div className={styles.selector}>
                     {selector === 'notice' ? (
                         <div className={styles.selectedButton}>
-                            공지사항
+                            공지
                         </div>) :
                         <div className={styles.selectButton}
                             onClick={() => setSelector('notice')}>
-                            공지사항
+                            공지
                         </div>}
                     {selector === 'free' ? (
                         <div className={styles.selectedButton}>
-                            자유게시판
+                            자유
                         </div>) :
                         <div className={styles.selectButton}
                             onClick={() => setSelector('free')}>
-                            자유게시판
+                            자유
                         </div>}
                     {selector === 'part' ? (
                         <div className={styles.selectedButton}>
-                            부서별게시판
+                            부서
                         </div>) :
                         <div className={styles.selectButton}
                             onClick={() => setSelector('part')}>
-                            부서별게시판
+                            부서
                         </div>}
                 </div>
                 <div className={styles.write}>
-                    <Link to={`/board/write/${selector}`}>글 작성</Link>
-
+                    <Link to={`/board/write/${selector}`} style={{ textDecoration: 'none' }}>글 작성</Link>
                 </div>
             </div>
-        </div >
+
+        </div>
     );
 }
 
