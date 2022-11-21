e27b665f726f435dae89a54f2797d46338c87ecb
yongwonkim1 <qwsa522@naver.com>
2022-11-21 21:58:52 +0900

    프로필

--- src/routes/Mafia.js
+++ src/routes/Mafia.js
@@ -1,33 +1,10 @@
 import {db} from '../firebase'
 import { collection, query, where, addDoc, getDocs } from "firebase/firestore"; 
 
-const bdagssf = async () => {
-    try {
-        const docRef = await addDoc(collection(db, "usersss"), {
-          first: "Adaaaaa",
-          last: "Lovelace",
-          born: 1815
-        });
-        console.log("Document written with ID: ", docRef.id);
-      } catch (e) {
-        console.error("Error adding document: ", e);
-      }
-}
-const asdsada = async () => {
-  const q = query(collection(db, "usersss"), where("first", "==", "Adaaaaa"));
-  const querySnapshot = await getDocs(q);
-  querySnapshot.forEach((doc) => {
-  console.log(`${doc.id} => ${doc.data()}`);
-});
-}
-
 function Mafia() {
     
     return (
-        <div>
-            <button onClick={() => bdagssf()}>djk</button>
-            <button onClick={() => asdsada()}>djk</button>
-        </div>
+      
     );
 }
 
